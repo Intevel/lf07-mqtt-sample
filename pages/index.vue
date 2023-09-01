3f409ee079ce4b2ebb0467d00b148cb273178b06
Conner Bachmann <github@conner-bachmann.de>
2023-09-01 12:34:40 +0200

    feat: add error

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,12 @@
     <div class="mt-8" v-if="temperature">
       <h2>Aktuelle Temperatur: {{ temperature }}</h2>
     </div>
+    <div class="mt-8" v-else>
+      <h2>Warte auf Nachrichten...</h2>
+    </div>
+    <div class="mt-8 text-red-500" v-if="error">
+    {{ error}}
+    </div>
   </div>
 </template>
 
@@ -19,20 +25,26 @@
 import * as mqtt from "mqtt/dist/mqtt.min";
 
 const temperature = ref("");
+const error = ref<string | undefined>(undefined);
 
 const url = "mqtt://172.20.199.158:9911";
 const topic = "test";
 
 const client = mqtt.connect("mqtt://172.20.199.158:9911");
 
-client.on("connect", function () {
+client.on("connect", function (err1) {
   console.log("Verbunden mit MQTT-Broker");
+    if (err1) {
+        error.value = `Fehler beim Verbinden mit dem MQTT-Broker: ${err1}`;
+        console.error(`Fehler beim Verbinden mit dem MQTT-Broker: ${err1}`);
+    }
 
   // Dem Topic beitreten
   client.subscribe(topic, function (err) {
     if (!err) {
       console.log(`Erfolgreich dem Topic ${topic} beigetreten`);
     } else {
+        error.value = `Fehler beim Beitritt zum Topic ${topic}: ${err}`;
       console.error(`Fehler beim Beitritt zum Topic ${topic}: ${err}`);
     }
   });
