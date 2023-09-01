13a06a084d4c6f87480805adccc9e23e7c227d09
Conner Bachmann <github@conner-bachmann.de>
2023-09-01 12:29:25 +0200

    feat: add  connection

---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex justify-end">
+    <img src="/frame.png" alt="QR-Code" class="w-16 h-16"/>
+  </div>
+  <div
+    class="flex justify-center h-screen items-center text-center flex-row md:flex-col"
+  >
+    <h1 class="font-extrabold text-2xl md:text-4xl mb-2">
+      Lernfeld 07 - MQTT Beispiel
+    </h1>
+    <p class="text-gray-800">Verbindet euch über den QR-Code</p>
+    <div class="mt-8" v-if="temperature">
+      <h2>Aktuelle Temperatur: {{ temperature }}</h2>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as mqtt from "mqtt/dist/mqtt.min";
+
+const temperature = ref("");
+
+const url = "mqtt://172.20.199.158:9911";
+const topic = "test";
+
+const client = mqtt.connect("mqtt://172.20.199.158:9911");
+
+client.on("connect", function () {
+  console.log("Verbunden mit MQTT-Broker");
+
+  // Dem Topic beitreten
+  client.subscribe(topic, function (err) {
+    if (!err) {
+      console.log(`Erfolgreich dem Topic ${topic} beigetreten`);
+    } else {
+      console.error(`Fehler beim Beitritt zum Topic ${topic}: ${err}`);
+    }
+  });
+});
+
+// Handler für eingehende Nachrichten
+client.on("message", function (topic, message) {
+  if (topic === "test") {
+    temperature.value = message.toString();
+    console.log(`Neue Nachricht im Topic ${topic}: ${message.toString()}`);
+  }
+});
+</script>
